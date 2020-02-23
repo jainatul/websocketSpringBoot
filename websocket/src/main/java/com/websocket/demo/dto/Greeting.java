63d8236231b4b13102549edc6294e721263c52ed
Atul Jain <atuldakhsana@gmail.com>
2020-02-24 00:56:40 +0530

    hello greeting websocket project

---
+++ websocket/src/main/java/com/websocket/demo/dto/Greeting.java
@@ -0,0 +1,14 @@
+package com.websocket.demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Greeting {
+
+  private String content;
+
+}
---
+++ websocket/src/main/java/com/websocket/demo/dto/HelloMessage.java
@@ -0,0 +1,9 @@
+package com.websocket.demo.dto;
+
+import lombok.Data;
+
+@Data
+public class HelloMessage {
+
+  private String name;
+}
